68acba10718c4b270f0ab1ee58c86270756cbc89
Macabe Daley <37773554+maca8e@users.noreply.github.com>
2025-07-22 00:09:22 +0200

    Rtd updates to book theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,10 +15,42 @@ release = '1.1.0'
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
-    "sphinx.ext.autodoc",
-    "sphinx.ext.napoleon",
-    "myst_nb",
+    "myst_nb",                      # MyST + notebook support
+    "sphinx.ext.autodoc",           # pull in docstrings
+    "sphinx.ext.napoleon",          # NumPy/Google style
+    "sphinx.ext.autosummary",       # generate API tables
+    "sphinx_copybutton",            # “copy” buttons on code blocks
+    "sphinx_autodoc_typehints",     # show Python 3 type hints
+    "sphinx_tabs.tabs",             # tabbed content
+    "sphinx_design",                # cards, grids, dropdowns
+    "sphinx.ext.intersphinx",       # links to external docs
+    "sphinxcontrib.bibtex",         # if you have a references.bib
+    "sphinx.ext.mathjax",           # render math
+    "IPython.sphinxext.ipython_console_highlighting",
+    "sphinxext.opengraph",          # social-media previews
 ]
+
+autosummary_generate = True
+autodoc_typehints = "description"
+autodoc_member_order = "groupwise"
+
+# MyST settings
+myst_enable_extensions = [
+    "amsmath", "colon_fence", "deflist", "dollarmath",
+    "html_image", "html_admonition",
+]
+myst_heading_anchors = 6
+
+# Bibtex
+bibtex_bibfiles = ["references.bib"]
+
+# Intersphinx targets
+intersphinx_mapping = {
+    "python":   ("https://docs.python.org/3", None),
+    "numpy":    ("https://numpy.org/doc/stable/",     None),
+    "pandas":   ("https://pandas.pydata.org/pandas-docs/stable/", None),
+    "scanpy":   ("https://scanpy.readthedocs.io/en/stable/", None),
+}
 
 templates_path = ['_templates']
 exclude_patterns = [
@@ -43,7 +75,21 @@ nb_execution_timeout = 90  # Set timeout to 90 seconds
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = "sphinx_book_theme"
 html_static_path = ['_static']
 
 html_logo = '_static/.lipinet_logo_v1_0051.png'
+html_favicon = '_static/.lipinet_logo_v1_0051.png'
+
+html_theme_options = {
+   "repository_url":        "https://github.com/saezlab/lipinet",
+   "use_repository_button": True,
+   "use_edit_page_button":  True,
+   "path_to_docs":          "docs/source",
+   "home_page_in_toc":      False,
+   "show_navbar_depth":     2,
+   # if you want the “Launch in Binder/Colab” buttons:
+   "launch_buttons": {
+     "colab_url":     "https://colab.research.google.com",
+   },
+ }
